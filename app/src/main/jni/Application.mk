80201ad439b72724b8d5c8e157021c3b5d7961fb
Your Name <you@example.com>
2020-05-25 10:16:42 +0800

    remove old files

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,6 +1,6 @@
 # Build both ARMv5TE and ARMv7-A machine code.
 APP_PLATFORM := android-9
-APP_ABI := armeabi armeabi-v7a
+APP_ABI := arm64-v8a armeabi-v7a
 APP_STL := gnustl_static
 #APP_STL := stlport_static
 APP_MODULES := cxcore cv com_graphics_NativeGraphics cpufeatures
